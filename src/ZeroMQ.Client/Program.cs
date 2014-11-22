17a10f0db36f750b0364385d34fabf7f4b0643e0
John van Loon <johnvanloon@gmail.com>
2014-11-22 11:56:06 +0200

    Problem: ReplySocket not waiting before publishing. Solution: Run both sockets in different tasks.

--- src/ZeroMQ.Client/Program.cs
+++ src/ZeroMQ.Client/Program.cs
@@ -10,32 +10,37 @@ namespace ZeroMQ.Client
         {
             using (NetMQContext context = NetMQContext.Create())
             {
-                Task clientTask = Task.Factory.StartNew(() => Client(context));
-                Task.WaitAll(clientTask);
+                Task listenTask = Task.Factory.StartNew(() => ListenToReplies(context));
+                Task broadcastTask = Task.Factory.StartNew(() => BroadcastAddress(context));
+                Task.WaitAll(listenTask, broadcastTask);
             }
         }
 
 
-        static void Client(NetMQContext context)
+        static void ListenToReplies(NetMQContext context)
         {
             using (NetMQSocket replySocket = context.CreateResponseSocket())
-            using (NetMQSocket pubSocket = context.CreatePublisherSocket())
             {
                 replySocket.Bind("tcp://*:5556");
-
-                for (int i = 0; i < 256; i++)
-                {
-                    pubSocket.Connect(string.Format("tcp://192.168.1.{0}:5555", i));
-                }
-                
-                
-                pubSocket.Send("John 192.168.1.4:5556");
 
                 while (true)
                 {
                     var read = replySocket.ReceiveString();
                     Console.WriteLine(read);
                 }
+            }
+        }
+
+        static void BroadcastAddress(NetMQContext context)
+        {
+            using (NetMQSocket pubSocket = context.CreatePublisherSocket())
+            {
+                for (int i = 1; i < 255; i++)
+                {
+                    pubSocket.Connect(string.Format("tcp://192.168.1.{0}:5555", i));
+                }
+
+                pubSocket.Send("John 192.168.1.4:5556");
             }
         }
     }
