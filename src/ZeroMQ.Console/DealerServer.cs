5e9311c00c20eee6c3d0e30883d6c34e7a973e73
Kristoffer Rolf Deinoff <kristoffer.deinoff@gmail.com>
2014-11-22 15:31:20 +0200

    Revert "FIXES #14"
    This reverts commit 05bdda9990ac249ee55e066ae9dea4c8455d5866.

--- src/ZeroMQ.Console/DealerServer.cs
+++ src/ZeroMQ.Console/DealerServer.cs
@@ -1,19 +1,14 @@
-﻿using System;
-using NetMQ;
+﻿using NetMQ;
 
 namespace ZeroMQ.Server
 {
     internal class DealerServer
     {
-        private const int MinPort = 5500;
-        private const int MaxPort = 5600;
-
         public void Start(NetMQContext context)
         {
             using (var serverSocket = context.CreateDealerSocket())
             {
-                var port = new Random().Next(MinPort, MaxPort);
-                serverSocket.Bind("tcp://*:" + port);
+                serverSocket.Bind("tcp://*:5556");
                 serverSocket.Send("My Secret");
 
                 while (true)
