b239a69a08266772d98a3a78798fa0da045bb184
John van Loon <jovanloon@deloitte.nl>
2014-11-22 09:57:24 +0200

    Working demo code.

---
+++ src/ZeroMQ.Client/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NetMQ;
+
+namespace ZeroMQ.Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (NetMQContext context = NetMQContext.Create())
+            {
+                Task clientTask = Task.Factory.StartNew(() => Client(context));
+                Task.WaitAll(clientTask);
+            }
+        }
+
+
+        static void Client(NetMQContext context)
+        {
+            using (NetMQSocket clientSocket = context.CreateRequestSocket())
+            {
+                clientSocket.Connect("tcp://127.0.0.1:5555");
+
+                while (true)
+                {
+                    System.Console.WriteLine("Please enter your message:");
+                    string message = System.Console.ReadLine();
+                    clientSocket.Send(message);
+
+                    string answer = clientSocket.ReceiveString();
+
+                    System.Console.WriteLine("Answer from server: {0}", answer);
+
+                    if (message == "exit")
+                    {
+                        break;
+                    }
+                }
+            }
+        }
+    }
+}
