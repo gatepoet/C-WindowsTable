68ee39c98a70a7055ba6e7c8f83eb706b99d1d88
John van Loon <johnvanloon@gmail.com>
2014-11-22 14:50:06 +0200

    Problem: We didn't have a simple solution for the RFC. Solution: Implement clean and easy solution for both the client and server.

---
+++ src/ZeroMQ.Console/DealerServer.cs
@@ -0,0 +1,21 @@
+﻿using NetMQ;
+
+namespace ZeroMQ.Server
+{
+    internal class DealerServer
+    {
+        public void Start(NetMQContext context)
+        {
+            using (var serverSocket = context.CreateDealerSocket())
+            {
+                serverSocket.Bind("tcp://*:5556");
+                serverSocket.Send("My Secret");
+
+                while (true)
+                {
+                    serverSocket.Send("Too late!");
+                }
+            }
+        }
+    }
+}
